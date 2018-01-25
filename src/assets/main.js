8dc2a19339897ef8bc945e1eceeabbbc27601324
GeorgVoronin <35695567+GeorgVoronin@users.noreply.github.com>
2018-01-25 09:28:14 +0100

    S1

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,4 +1,11 @@
 $(function() {
+  $.ajax('https://www.codeschool.com/users/anka-adesso.json', {
+    dataType: 'jsonp'
+    success: function(response) {
+      //handle
+    }
+
+  })
 
   // your code will go here
 
